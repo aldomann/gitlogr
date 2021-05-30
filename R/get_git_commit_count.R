2425136d6faf92bea59b2cce7fb0ddacfe6be7ac
Alfredo Hernández <aldomann.designs@gmail.com>
2021-05-30 16:59:58 +0100

    Minor namespace cleanup

--- R/get_git_commit_count.R
+++ R/get_git_commit_count.R
@@ -1,26 +1,25 @@
 #' Get Git commit count
 #'
-#' Get your Git commit count in a single date or range of dates
+#' Get your Git commit count in a single date or range of dates.
 #'
-#' @param from Date to count commits from
-#' @param to Optional ending date if you want to count commits in a date range
-#' @param author Optional author of the commits
-#' @param path Optional path of your Git reporitory
+#' @param from Date to count commits from.
+#' @param to Optional ending date if you want to count commits in a date range.
+#' @param author Optional author of the commits.
+#' @param path Optional path of your Git repository.
 #'
 #' @keywords git
 #' @export
 #' @examples
 #' get_git_commit_count(from = "2019-06-01")
-
 get_git_commit_count <- function(from, to = NULL, author = NULL, path = NULL) {
   if (is.null(to)) {
     date_range <- from
   } else {
-    date_range <- base::seq(base::as.Date(from), base::as.Date(to), 1) %>%
-      base::as.character()
+    date_range <- seq(as.Date(from), as.Date(to), 1) %>%
+      as.character()
   }
 
-  commit_count <- gitlogr::get_git_commit_history(
+  commit_count <- get_git_commit_history(
     from = from,
     to = to,
     author = author,
@@ -31,9 +30,9 @@ get_git_commit_count <- function(from, to = NULL, author = NULL, path = NULL) {
     dplyr::summarise("count" = dplyr::n()) %>%
     dplyr::filter(date %in% date_range) %>%
     dplyr::pull("count") %>%
-    base::sum()
+    sum()
 
-  if (base::length(commit_count) == 0) {
+  if (length(commit_count) == 0) {
     commit_count <- 0
   }
 
