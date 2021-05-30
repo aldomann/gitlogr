eff95421a8f5a22b0a9046164df8e7971413375d
Alfredo Hernández <aldomann.designs@gmail.com>
2021-05-30 17:30:53 +0100

    Using rlang::.data

--- R/get_git_commit_count.R
+++ R/get_git_commit_count.R
@@ -8,9 +8,10 @@
 #' @param path Optional path of your Git repository.
 #'
 #' @keywords git
-#' @export
 #' @examples
 #' get_git_commit_count(from = "2019-06-01")
+#' @importFrom rlang .data
+#' @export
 get_git_commit_count <- function(from, to = Sys.Date(), author = NULL, path = NULL) {
   commit_count <- get_git_commit_history(
     from = from,
@@ -18,13 +19,13 @@ get_git_commit_count <- function(from, to = Sys.Date(), author = NULL, path = NU
     author = author,
     path = path
   ) %>%
-    tidyr::separate(date, into = c("date", "time"), sep = " ") %>%
-    dplyr::group_by(date) %>%
-    dplyr::summarise("count" = dplyr::n()) %>%
+    tidyr::separate(.data$date, into = c("date", "time"), sep = " ") %>%
+    dplyr::group_by(.data$date) %>%
+    dplyr::summarise(count = dplyr::n()) %>%
     dplyr::filter(
-      dplyr::between(as.Date(date), as.Date(from), as.Date(to))
+      dplyr::between(as.Date(.data$date), as.Date(from), as.Date(to))
     ) %>%
-    dplyr::pull("count") %>%
+    dplyr::pull(.data$count) %>%
     sum()
 
   if (length(commit_count) == 0) {
--- R/get_git_commit_history.R
+++ R/get_git_commit_history.R
@@ -9,9 +9,10 @@
 #' @param path Optional path of your Git repository.
 #'
 #' @keywords git
-#' @export
 #' @examples
 #' get_git_commit_history(from = "2019-06-01")
+#' @importFrom rlang .data
+#' @export
 get_git_commit_history <- function(from, to = Sys.Date(), author = NULL, path = NULL, raw = FALSE) {
 
   # Parse path
@@ -39,7 +40,6 @@ get_git_commit_history <- function(from, to = Sys.Date(), author = NULL, path = 
     " --reverse",
     " --pretty='%Cgreen %cI ## %Creset %s'"
   )
-  message(call)
 
   # Get commits
   git_commits <- system(call, intern = TRUE) %>%
@@ -50,16 +50,16 @@ get_git_commit_history <- function(from, to = Sys.Date(), author = NULL, path = 
       lines = git_commits
     ) %>%
       tidyr::separate(
-        col = "lines",
+        col = .data$lines,
         into = c("date", "message"),
         sep = "##"
       ) %>%
       dplyr::mutate(
-        date = lubridate::ymd_hms(date),
-        message = stringr::str_trim(message, side = "left")
+        date = lubridate::ymd_hms(.data$date),
+        message = stringr::str_trim(.data$message, side = "left")
       ) %>%
       dplyr::filter(
-        dplyr::between(as.Date(date), as.Date(from), as.Date(to))
+        dplyr::between(as.Date(.data$date), as.Date(from), as.Date(to))
       )
   }
 
