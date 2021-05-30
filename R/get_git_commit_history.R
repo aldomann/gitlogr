5c22dd9cc520ed9e9bbfb3f0c245f63a31406bc1
Alfredo Hernández <aldomann.designs@gmail.com>
2021-05-30 16:59:35 +0100

    get_git_commit_history() uses {glue} for constructing system call

--- R/get_git_commit_history.R
+++ R/get_git_commit_history.R
@@ -1,41 +1,62 @@
 #' Get Git commit history
 #'
-#' Get your Git commit history as a data frame or raw text between two specified dates
+#' Get your Git commit history as a data frame or raw text between two specified dates.
 #'
-#' @param from Starting date
-#' @param to Ending date
-#' @param author Optional author of the commits
-#' @param raw Whether the output is a raw string vector (TRUE), or a data frame (FALSE)
-#' @param path Optional path of your Git reporitory
+#' @param from Starting date.
+#' @param to Ending date.
+#' @param author Optional author of the commits.
+#' @param raw Whether the output is a raw string vector (\code{TRUE}), or a data frame (\code{FALSE}).
+#' @param path Optional path of your Git repository.
 #'
 #' @keywords git
 #' @export
 #' @examples
 #' get_git_commit_history(from = "2019-06-01")
-
 get_git_commit_history <- function(from, to = Sys.Date(), author = NULL, path = NULL, raw = FALSE) {
-  git_commits <- system(
-    paste(
-      "git",
-      ifelse(is.null(path), "", paste0("--git-dir ", path %>% stringr::str_remove("/$"), "/.git")),
-      "log",
-      ifelse(is.null(author), "", paste("--author", author)),
-      "--since", lubridate::as_date(from) - 1,
-      "--until", to,
-      "--reverse",
-      "--pretty='%Cgreen %cI ## %Creset %s'"
-    ),
-    intern = TRUE
-  ) %>%
+
+  # Parse path
+  path <- ifelse(
+    is.null(path),
+    "",
+    glue::glue(" --git-dir {stringr::str_remove(path, '/$')}/.git")
+  )
+
+  # Parse author
+  author <- ifelse(
+    is.null(author),
+    "",
+    glue::glue(" --author {author}")
+  )
+
+  # Construct system call
+  call <- glue::glue(
+    "git",
+    path,
+    " log",
+    author,
+    " --since {lubridate::as_date(from) - 1}",
+    " --until {to}",
+    " --reverse",
+    " --pretty='%Cgreen %cI ## %Creset %s'"
+  )
+
+  message(call)
+
+  git_commits <- system(call, intern = TRUE) %>%
     stringr::str_trim(side = "left")
 
   if (!raw) {
     git_commits <- data.frame(
       lines = git_commits
     ) %>%
-      tidyr::separate("lines", c("date", "message"), sep = "##") %>%
+      tidyr::separate(
+        col = "lines",
+        into = c("date", "message"),
+        sep = "##"
+      ) %>%
       dplyr::mutate(
-        date = lubridate::ymd_hms(date)
+        date = lubridate::ymd_hms(date),
+        message = stringr::str_trim(message, side = "left")
       )
   }
 
